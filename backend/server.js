b498329eb60c9bf60f6f49e1f1b905e9c527556d
Md Faiz Ansari <mdfaizansari9262@gmail.com>
2026-01-30 20:20:43 +0530

    Phase2 changes

--- backend/server.js
+++ backend/server.js
@@ -30,6 +30,6 @@ mongoose
   res.json([]);
 });
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+app.listen(process.env.PORT, () => {
+  console.log("Server running" );
 });
